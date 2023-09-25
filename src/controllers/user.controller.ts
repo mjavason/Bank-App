048740f7c89fe9de40fb2df606e63f6cff00914a
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2023-09-25 21:11:03 +0100

    Add routes for autn, user, bank and transaction

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -59,15 +59,96 @@ class UserController {
     // Passwords match, user is authenticated
     const { _id, role } = user;
     let accessToken = await signJwt({ _id, role, username }, ACCESS_TOKEN_SECRET, '48h');
-    let refreshToken = await signJwt({_id, role, username}, REFRESH_TOKEN_SECRET, '24h');
+    let refreshToken = await signJwt({ _id, role, username }, REFRESH_TOKEN_SECRET, '24h');
 
     let data = {
       accessToken: accessToken,
-      refreshToken: refreshToken
+      refreshToken: refreshToken,
     };
 
     // Return a success response or the token, depending on your authentication method
     return SuccessResponse(res, data, MESSAGES.LOGGED_IN);
+  }
+
+  async resetPasswordMail(req: Request, res: Response) {
+    // // try {
+    //   const { email } = req.body;
+
+    //   // Find the user by email
+    //   const user = await userService.find({ email });
+
+    //   if (!user) {
+    //     return res.status(404).json({ message: 'User not found' });
+    //   }
+
+    //   // Generate a unique reset token
+    //   const token = crypto.randomBytes(32).toString('hex');
+
+    //   // Set the expiration date to 1 hour from now
+    //   const expiresAt = new Date();
+    //   expiresAt.setHours(expiresAt.getHours() + 1);
+
+    //   // Save the reset token to the database
+    //   const resetToken = new ResetToken({ user: user._id, token, expiresAt });
+    //   await resetToken.save();
+
+    //   // Send the reset email
+    //   const transporter = nodemailer.createTransport({
+    //     // Configure your email provider here (e.g., Gmail, SMTP)
+    //   });
+
+    //   const resetLink = `https://yourwebsite.com/reset-password/${token}`;
+
+    //   const mailOptions = {
+    //     from: 'your@email.com',
+    //     to: email,
+    //     subject: 'Password Reset Request',
+    //     text: `Click the following link to reset your password: ${resetLink}`,
+    //   };
+
+    //   await transporter.sendMail(mailOptions);
+
+    //   res.status(200).json({ message: 'Password reset email sent' });
+    // // } catch (error) {
+    // //   console.error(error);
+    // //   res.status(500).json({ message: 'Server error' });
+    // // }
+
+    return SuccessMsgResponse(res);
+  }
+
+  async resetPassword(req: Request, res: Response){
+    // try {
+    //   const { token } = req.params;
+    //   const { newPassword } = req.body;
+  
+    //   // Find the reset token in the database
+    //   const resetToken = await ResetToken.findOne({ token });
+  
+    //   if (!resetToken || resetToken.expiresAt < new Date()) {
+    //     return res.status(400).json({ message: 'Invalid or expired token' });
+    //   }
+  
+    //   // Find the associated user and update their password
+    //   const user = await User.findById(resetToken.user);
+  
+    //   if (!user) {
+    //     return res.status(404).json({ message: 'User not found' });
+    //   }
+  
+    //   user.password = newPassword;
+    //   await user.save();
+  
+    //   // Delete the used reset token
+    //   await resetToken.remove();
+  
+    //   res.status(200).json({ message: 'Password reset successful' });
+    // } catch (error) {
+    //   console.error(error);
+    //   res.status(500).json({ message: 'Server error' });
+    // }
+
+    return SuccessMsgResponse(res);
   }
 }
 
